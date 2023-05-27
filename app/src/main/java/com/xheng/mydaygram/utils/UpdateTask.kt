6587748bb2377b5e487e496db70c6404c6ffbe3b
Xheng222 <xheng222@163.com>
2023-05-27 21:07:10 +0800

    Update 1

--- app/src/main/java/com/xheng/mydaygram/utils/UpdateTask.kt
+++ app/src/main/java/com/xheng/mydaygram/utils/UpdateTask.kt
@@ -1,7 +1,39 @@
 package com.xheng.mydaygram.utils
 
+import android.util.Log
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.MalformedURLException
 import java.net.URL
 
 class UpdateTask {
     private val url = URL("http://127.0.0.1")
+
+    private fun getJson(): JSONObject {
+        val str = StringBuilder()
+        try {
+            val uc = url.openConnection()
+            val read = BufferedReader(InputStreamReader(uc.getInputStream(), "UTF-8"))
+            var inputLine: String? = null
+            inputLine = read.readLine()
+            while (inputLine != null) {
+                str.append(inputLine)
+                inputLine = read.readLine()
+            }
+            read.close()
+        } catch (e: MalformedURLException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return JSONObject(str.toString())
+    }
+
+    fun checkJSON() {
+        val json = getJson()
+        Log.e("MyDayGram", json.getString("version"))
+    }
 }
